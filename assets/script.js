b2c770372caa8a5442ebfd4bd2ce85c2b7d2981c
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-07 12:43:52 -0500

    module scope and benefits like import

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,18 @@
+import MediaPlayer from './mediaPlayer.js';
+
 const video = document.querySelector('video');
 const button = document.querySelector('button');
+
+const player = new MediaPlayer({ el: video });
 
 /*
 // Funciona, pero no es muy extensible
 button.onclick = () => video.play();
 // Por eso desarrollamos una clase (obj en js) MediaPlayer
-*/
-
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function() {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause();
-};
-
-/*
-MediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-};
 
 button.onclick = () => player.togglePlay();
 */
 
-const player = new MediaPlayer({ el: video });
-
-
 button.onclick = () => {
-  player.media.paused? player.play() : player.pause()
+    player.media.paused? player.play() : player.pause()
 }
