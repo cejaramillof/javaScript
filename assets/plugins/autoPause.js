6a2abc1ee0f9a56c2dc089c1724a26f16b1bad09
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-21 15:42:13 -0500

    stop and start with visibility change

--- assets/plugins/autoPause.js
+++ assets/plugins/autoPause.js
@@ -2,17 +2,21 @@ class AutoPause {
     constructor() {
         this.threshold = 0.25;        
         this.handleInterseption = this.handleInterseption.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
 
     run(player) {
         this.player = player;
         this.pausedByScroll = false;
+        this.pausedByTab = false;
         // const observer = new IntersectionObserver(handler, config)
         const observer = new IntersectionObserver(
             // threshold: umbral - define que % del elemento tiene que tener interseccion
             this.handleInterseption, { threshold: this.threshold }
         );
         observer.observe(this.player.media);
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
 
     handleInterseption(entries) {
@@ -25,6 +29,19 @@ class AutoPause {
             this.player.play();
             this.pausedByScroll = false;
         }         
+    }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === "visible";
+
+        if ( !isVisible && !this.player.media.paused ) {
+            this.player.pause();  
+            this.pausedByTab = true;
+        }
+        if ( isVisible && this.pausedByTab ) {
+            this.player.play();
+            this.pausedByTab = false;
+        }
     }
 }
 export default AutoPause;
