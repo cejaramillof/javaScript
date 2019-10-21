3f1ca8b474ff899f304117ab45297c180315b583
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-21 16:15:10 -0500

    offline with service workers

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,9 @@ playButton.onclick = () => {
 }
 
 muteButton.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch( err => {
+        console.log(err.message);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,40 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if (request.method !== 'GET') {
+        return;
+    }
+    event.respondWith(cachedResponse(request));
+    event.waitUntil(updateCache(request));
+})
+
+async function precache () {
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
+        '/',
+        './index.html',
+        './assets/BigBuckBunny.mp4',
+        './assets/style.css',         
+        './assets/script.js',
+        './assets/mediaPlayer.js',
+        './assets/plugins/autoPause.js',
+        './assets/plugins/autoPlay.js'
+    ]);
+}
+
+async function cachedResponse (request) {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
