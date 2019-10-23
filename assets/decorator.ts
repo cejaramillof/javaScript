6df962e401e60ab9a9ad2163aa8324b83ceaae9f
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-23 14:40:23 -0500

    decorator example

---
+++ assets/decorator.ts
@@ -0,0 +1,55 @@
+class Field {
+    errors: string[];
+    input: HTMLInputElement;
+  
+    constructor(input: HTMLInputElement) {
+      this.input = input;
+      this.errors = [];
+  
+      let errorMessage = document.createElement("p");
+      errorMessage.className = "text-danger";
+      this.input.parentNode.insertBefore(errorMessage, this.input.nextSibling);
+  
+      this.input.addEventListener("input", () => {
+        this.errors = [];
+        this.validate();
+        errorMessage.innerText = this.errors[0] || "";
+      });
+    }
+  
+    validate() {}
+  }
+  
+  function RequiredFieldDecorator(field: Field): Field {
+    let validate = field.validate;
+  
+    field.validate = function() {
+      validate();
+      let value = field.input.value;
+      if (!value) {
+        field.errors.push("Requerido");
+      }
+    };
+  
+    return field;
+  }
+  
+  function EmailFieldDecorator(field: Field): Field {
+    let validate = field.validate;
+  
+    field.validate = function() {
+      validate();
+      let value = field.input.value;
+  
+      if (value.indexOf("@") === -1) {
+        field.errors.push("Debe ser un email");
+      }
+    };
+  
+    return field;
+  }
+  
+  let field = new Field(document.querySelector("#value"));
+  field = RequiredFieldDecorator(field);
+  field = EmailFieldDecorator(field);
+  
